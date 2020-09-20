ab1bcfd687537dc69afc136fbddfeaee79389c8e
Filip Tanic <tanic.filip.97@gmail.com>
2020-09-20 21:20:59 +0200

    Redirected output to result file

--- openmp/backprop/run_benchmark.sh
+++ openmp/backprop/run_benchmark.sh
@@ -10,6 +10,6 @@ do
    seconds="$(echo $seconds | tr , .)"
    realSecondsElapsed="$(echo "$realSecondsElapsed + $minutes * 60.0 + $seconds" | bc -l)"
 done
-echo "Total wall clock run time: $realSecondsElapsed"
+echo "Total wall clock run time: $realSecondsElapsed" &> result
 avgRuntime="$(echo "$realSecondsElapsed / $runs" | bc -l)"
-echo "Average wall clock time of single run: $avgRuntime"
+echo "Average wall clock time of single run: $avgRuntime" &>> result
